23d9ae51f4dd07defc64489739984ab0bb7c6d65
JayeeHong <hjy48486@naver.com>
2021-03-25 10:33:33 +0900

    06. Queue 구현하기

---
+++ java-objective-programming/Chapter5/src/ch06/MyListQueueTest.java
@@ -0,0 +1,15 @@
+package ch06;
+
+public class MyListQueueTest {
+    public static void main(String[] args) {
+        MyLinkedQueue linkedQueue = new MyLinkedQueue();
+        linkedQueue.enQueue("A");
+        linkedQueue.enQueue("B");
+        linkedQueue.enQueue("C");
+
+        linkedQueue.printAll();
+
+        System.out.println(linkedQueue.deQueue());
+        System.out.println(linkedQueue.deQueue());
+    }
+}
