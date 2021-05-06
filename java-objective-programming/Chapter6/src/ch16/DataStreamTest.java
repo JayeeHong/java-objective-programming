aea86edc1d46f0356354b36741ba0305c404aabe
JayeeHong <hjy48486@naver.com>
2021-05-06 16:52:50 +0900

    16. 여러가지 보조 스트림 클래스

---
+++ java-objective-programming/Chapter6/src/ch16/DataStreamTest.java
@@ -0,0 +1,35 @@
+package ch16;
+
+import java.io.*;
+
+public class DataStreamTest {
+    public static void main(String[] args) {
+
+        try (FileOutputStream fos = new FileOutputStream("data.txt");
+             DataOutputStream dos = new DataOutputStream(fos)) {
+
+            dos.writeByte(100);
+            dos.writeChar('A');
+            dos.writeInt(10);
+            dos.writeFloat(3.14f);
+            dos.writeUTF("TEST");
+
+        } catch (IOException e) {
+            System.out.println(e);
+        }
+
+        try (FileInputStream fis = new FileInputStream("data.txt");
+             DataInputStream dis = new DataInputStream(fis)) {
+
+            //쓴대로 읽어야 함
+            System.out.println(dis.readByte());
+            System.out.println(dis.readChar());
+            System.out.println(dis.readInt());
+            System.out.println(dis.readFloat());
+            System.out.println(dis.readUTF());
+            
+        } catch (IOException e) {
+            System.out.println(e);
+        }
+    }
+}
